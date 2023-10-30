a2129a878bf5d1c3dd202938da21c07a46393dd8
Copperyz <nahaisi96@gmail.com>
2023-10-30 15:22:28 +0200

    Update app-user-list.js

--- public/assets/js/app-user-list.js
+++ public/assets/js/app-user-list.js
@@ -39,7 +39,7 @@ $(function () {
   // Users datatable
   if (dt_user_table.length) {
     var dt_user = dt_user_table.DataTable({
-      ajax: 'get-userss', // JSON file to add data
+      ajax: 'get-users', // JSON file to add data
       columns: [
         // columns according to JSON
         { data: '' },

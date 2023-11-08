ba344774d05b3d610c686d1ab43301feb49113d9
Copperyz <nahaisi96@gmail.com>
2023-11-08 20:41:13 +0200

    More on orders

--- public/assets/js/app-invoice-add.js
+++ public/assets/js/app-invoice-add.js
@@ -99,17 +99,22 @@ $(function () {
     sourceItem.on('submit', function (e) {
       e.preventDefault();
     });
+
     sourceItem.repeater({
       show: function () {
         // Increment the counter for unique IDs
         var counter = $(this).closest('[data-repeater-list]').find('[data-repeater-item]').length;
 
-        console.log(counter)
-        // Increment IDs for selects
-        $(this).find('select[name="parcel_types_id"]').attr('id', 'parcel_types_id_' + counter);
-        $(this).find('select[name="good_types_id"]').attr('id', 'good_types_id_' + counter);
+        // Set unique IDs for selects before they are added
+        var parcelSelect = $(this).find('select[name="parcel_types_id"]');
+        var goodSelect = $(this).find('select[name="good_types_id"]');
+        parcelSelect.attr('id', 'parcel_types_id_' + counter).addClass('select2');
+        goodSelect.attr('id', 'good_types_id_' + counter).addClass('select2');
 
         $(this).slideDown();
+
+        // Manually initialize Select2 for both old and new selects
+        initializeSelect2();
 
         // Initialize tooltip on load of each item
         const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
@@ -121,6 +126,13 @@ $(function () {
         $(this).slideUp();
       }
     });
+
+    // Explicitly initialize Select2 for existing selects
+    initializeSelect2();
+
+    function initializeSelect2() {
+      $('select.select2').select2({ allowClear: true });
+    }
   }
 
 
@@ -130,10 +142,60 @@ $(function () {
     // Trigger the form submission when the button is clicked
     var formData = $("#addOrderForm").serializeArray();
     console.log(formData)
+    $.ajax({
+      url: '../orders',
+      method: 'POST',
+      data: $("#addOrderForm").serialize(),
+      headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+      },
+      dataType: 'json',
+      processData: false,
+      success: function (response, status, xhr) {
+        if (xhr.status === 200) {
+          // Handle a successful response
+          // $("#editPermissionForm").trigger('reset');
+          // dt_permission.ajax.url('get-permissions').load();
+          Swal.fire({
+            icon: 'success',
+            title: '',
+            text: response.message,
+            confirmButtonText: doneTranslation,
+            customClass: {
+              confirmButton: 'btn btn-success'
+            }
+          });
+          $('#editPermissionModal').modal('hide');
+
+        } else {
+          // Handle other status codes
+        }
+      },
+      error: function (response, xhr, status, error) {
+        // Handle the error response here
+        var errorMessages = Object.values(response.responseJSON.errors).flat();
+        // Format error messages with line breaks
+        var formattedErrorMessages = errorMessages.join('<br>'); // Join the error messages with <br> tags
+        // Create the Swal alert
+        Swal.fire({
+          title: response.responseJSON.message,
+          html: formattedErrorMessages,
+          icon: 'error',
+          customClass: {
+            confirmButton: 'btn btn-primary'
+          },
+          buttonsStyling: false
+        });
+      }
+    });
   });
 
   $(".cancelButton").on("click", function (event) {
     // Trigger the form submission when the button is clicked
     $("#addOrderForm").trigger("reset");
+    $("#orderItems").empty();
   });
+
+
+
 });

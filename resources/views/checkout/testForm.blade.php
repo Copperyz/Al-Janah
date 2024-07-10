3ec4706ad4640e5a9111b2d19cb4d36ac16433ec
Copperyz <nahaisi96@gmail.com>
2024-07-10 14:07:43 +0200

    Update testForm.blade.php

--- resources/views/checkout/testForm.blade.php
+++ resources/views/checkout/testForm.blade.php
@@ -13,7 +13,7 @@
 
         <div class="container mt-5">
             <div class="text-center mt-5">
-                <button class="btn btn-success" type="submit">Simulate Redirection</button>
+                <button class="btn btn-success" type="submit">Simulate Redirectiox</button>
             </div>
         </div>
     </form>

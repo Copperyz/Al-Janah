3af126da256f5628c27d72eb541739773695184c
Copperyz <nahaisi96@gmail.com>
2024-10-15 20:52:53 +0200

    change include js files

--- resources/views/layouts/sections/scripts.blade.php
+++ resources/views/layouts/sections/scripts.blade.php
@@ -13,7 +13,7 @@
 <!-- END: Page Vendor JS-->
 <!-- BEGIN: Theme JS-->
 <script src="{{asset('assets/vendor/libs/block-ui/block-ui.js')}}"></script>
-<script src="{{asset('assets/js/extended-ui-blockui.js')}}"></script>
+<script src="{{asset('assets/js/extends/extended-ui-blockui.js')}}"></script>
 <script src="{{asset('assets/vendor/libs/sweetalert2/sweetalert2.js')}}"></script>
 <script src="{{ asset('assets/js/main/main.js') }}"></script>
 <script>
--- resources/views/layouts/sections/scriptsIncludes.blade.php
+++ resources/views/layouts/sections/scriptsIncludes.blade.php
@@ -11,7 +11,7 @@ $menuCollapsed = ($configData['menuCollapsed'] === 'layout-menu-collapsed') ? js
 @endif
 
   <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
-  <script src="{{ asset('assets/js/config.js') }}"></script>
+  <script src="{{ asset('assets/js/extends/config.js') }}"></script>
 
 @if ($configData['hasCustomizer'])
   <script>
